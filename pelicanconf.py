afb2cfccdf00b601c4afd2147ad66bf873978033
thavan <thavanathan.t@gmail.com>
2014-11-06 18:01:04 +0530

    updated about me

--- pelicanconf.py
+++ pelicanconf.py
@@ -20,13 +20,10 @@ TRANSLATION_FEED_ATOM = None
 
 # Blogroll
 LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+         ('Python.org', 'http://python.org/'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('Google+', 'https://plus.google.com/u/0/+ThavanathanT/'),)
 
 DEFAULT_PAGINATION = 10
 
