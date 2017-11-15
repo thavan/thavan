8dd732e22358d0fd34b194e1cf29fc1a7a800aed
thavan <thavanathan.t@gmail.com>
2017-11-15 08:13:56 +0530

    added new post

---
+++ _site/content/2015-02-10-an-update-to-android-app.md
@@ -0,0 +1,12 @@
+Title: An update to TamilNames Android app
+Date: 2015-02-10 09:00:00
+Modified: 2015-02-10 09:00:00
+Slug: an-update-to-tamilnames-android-app
+Category: Android
+Tags: android, tamil-names
+
+I have published an update to TamilNames android app this week. This update includes the ability to search names by starting letters and gender. I've planned to include few more usability improvements in future.
+
+Grab it from [here][app-link].
+
+[app-link]: https://play.google.com/store/apps/details?id=me.thava.babynames
---
+++ _site/pelicanconf.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Thavanathan T'
+SITENAME = u'Thavanathan'
+SITEURL = ''
+
+PATH = 'content'
+STATIC_PATHS = ['assets']
+
+TIMEZONE = 'Europe/Paris'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+
+# Blogroll
+#LINKS = (('Pelican', 'http://getpelican.com/'),
+#         ('Python.org', 'http://python.org/'),)
+
+# Social widget
+SOCIAL = (('Google+', 'https://plus.google.com/u/0/+ThavanathanT/'),
+          ('Facebook', 'http://facebook.com/thavanathan'),
+          ('LinkedIn', 'https://www.linkedin.com/in/thavanathan'))
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+GOOGLE_ANALYTICS = 'UA-63651774-1'
+
+DELETE_OUTPUT_DIRECTORY = False
+
+THEME = "themes/notmyidea"
