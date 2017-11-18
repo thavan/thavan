d04fc3e1c1efe818905065ea49bfe0e44d0fc0b8
thavan <thavanathan.t@gmail.com>
2017-11-19 03:01:59 +0530

    updated

--- _site/publish.sh
+++
@@ -1,9 +0,0 @@
-#rm -rf /Users/thavan/workspace/www.thavanathan.com.appengine/www
-#rm -rf /Users/thavan/workspace/www.thavanathan.com.appengine/public
-
-#cp -r /Users/thavan/workspace/www.thavanathan.com/_site /Users/thavan/workspace/www.thavanathan.com.appengine/www
-#cp -r /Users/thavan/workspace/www.thavanathan.com/public /Users/thavan/workspace/www.thavanathan.com.appengine/public
-
-#cd /Users/thavan/workspace/www.thavanathan.com.appengine
-#gcloud app deploy
-scp -r _site/* thavan@thavanathan.com:/var/www/www.thavanathan.com/
